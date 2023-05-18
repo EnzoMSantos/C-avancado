5b07da3a00ad23789d150c8299da2342033cd96d
EnzoMSantos <enzosantos2502@gmail.com>
2023-05-18 19:43:48 -0300

    agr da p chutar a letra

--- forca.c
+++ forca.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char letra1 = 'M';
-    char letra2 = 'E';
-    char letra3 = 'L';
-    char letra4 = 'A';
-    char letra5 = 'N';
-    char letra6 = 'C';
-    char letra7 = 'I';
-    char letra8 = 'A';
+    char palavraSecreta[20];
+
+    sprintf(palavraSecreta, "MELANCIA");
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    do {
+    char chute;
+
+    printf("Qual letra? ");
+    scanf("%c", &chute);
+
+    for(int i = 0; i < strlen(palavraSecreta); i++) {
+        if(palavraSecreta[i] == chute) {
+            printf("A posicao %d tem essa letra\n", i+1);
+        }
+    }
+} while (!acertou && !enforcou);
+        
+    
+    
 
     
 }
